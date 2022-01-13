d26a53a47f75717245d52ec52a076f7eb2c3901d
Jina Kim <fghjiurt@naver.com>
2022-01-13 20:41:09 +0900

    admin 설정

--- mascot/models.py
+++ mascot/models.py
@@ -33,7 +33,7 @@ class Region(models.TextChoices):
 
 class Character(TimestampedModel):
     region = models.CharField(max_length=10, db_index=True, verbose_name="지역")
-    city = models.CharField(max_length=10, db_index=True, verbose_name="도시")
+    city = models.CharField(max_length=10, blank=True, db_index=True, verbose_name="시·군·구")
     charming = models.PositiveSmallIntegerField(
         validators=[
             MaxValueValidator(5),
