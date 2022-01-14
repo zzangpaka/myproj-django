a4857b49517b55f45ea4e35aa9b3552049605ea0
Jina Kim <fghjiurt@naver.com>
2022-01-14 10:29:11 +0900

    api설정 및 list를 json형식으로 출력

--- mascot/views.py
+++ mascot/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from rest_framework import viewsets
+from mascot.serializers import CharacterSerializer
+from mascot.models import Character
+
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+
+def character_list(request):
+    qs = Character.objects.all()
+    data = [
+        {
+            "id": character.id,
+            "name": character.name,
+            "explain": character.explain
+        }
+        for character in qs
+    ]
+    json_string = json.dumps(data)
+    return HttpResponse(json_string)
